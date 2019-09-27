b5ce6001217f7e94c1659f7c1686bd7b965a787f
Lukas Elmer <lukas.elmer@gmail.com>
2019-09-28 01:26:06 +0200

    For the statistics only

--- plantree-web/src/App.tsx
+++ plantree-web/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +22,3 @@ const App: React.FC = () => {
     </div>
   );
 }
-
-export default App;

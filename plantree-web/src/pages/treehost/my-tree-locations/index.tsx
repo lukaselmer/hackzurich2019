dbe2e914383f69f0d72f9f8f1faa7cbdd350e2e3
Lukas Elmer <lukas.elmer@gmail.com>
2019-09-28 16:11:52 +0200

    For the sake of my sanity, just ignore this...

--- plantree-web/src/pages/treehost/my-tree-locations/index.tsx
+++ plantree-web/src/pages/treehost/my-tree-locations/index.tsx
@@ -4,14 +4,5 @@ import { TeamBiosSection } from '../../../components/TeamBiosSection';
 import './styles.scss';
 
 export function MyTreeLocations(_props: unknown) {
-  return (
-    <>
-      <Section color="primary" size="large" title="My tree locations">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequatur numquam aliquam
-        tenetur ad amet inventore hic beatae, quas accusantium perferendis sapiente explicabo, corporis
-        totam!
-      </Section>
-      <TeamBiosSection color="white" size="medium" title="Meet the Team" subtitle="" />
-    </>
-  );
+  return <>Hello, World</>;
 }

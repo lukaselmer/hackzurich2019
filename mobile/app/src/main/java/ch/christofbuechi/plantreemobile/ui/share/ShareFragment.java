f36b597d8162f2901a10ec01fb3d553fd2127042
Christof Buechi <christof.buechi@gmail.com>
2019-09-28 23:01:03 +0200

    share - just text

--- mobile/app/src/main/java/ch/christofbuechi/plantreemobile/ui/share/ShareFragment.java
+++ mobile/app/src/main/java/ch/christofbuechi/plantreemobile/ui/share/ShareFragment.java
@@ -7,11 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 import antonkozyriatskyi.circularprogressindicator.CircularProgressIndicator;
 import ch.christofbuechi.plantreemobile.R;
@@ -40,15 +45,13 @@ public class ShareFragment extends Fragment {
         circularProgress.setDotColor(getResources().getColor(R.color.colorPrimaryDark));
 
 
-        TextView shareView = view.findViewById(R.id.text_share_on_facebook);
+        final TextView shareView = view.findViewById(R.id.text_share_on_facebook);
         shareView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri imageUri = Uri.parse("android.resource://" + getActivity().getPackageName() + "/drawable/" + "ic_launcher");
                 Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-                sharingIntent.setType("image/*");
-                sharingIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
-                sharingIntent.putExtra(Intent.EXTRA_TEXT, "I did 5000 - halftime!! what about you?");
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, "WOW - I did 5000 - halftime!! what about you?");
                 sharingIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 startActivity(Intent.createChooser(sharingIntent, "Share via"));
             }

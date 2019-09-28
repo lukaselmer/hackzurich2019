949a8b79e5886831e32da533797a1cb59a6f7406
Lukas Elmer <lukas.elmer@gmail.com>
2019-09-28 11:25:19 +0200

    Simplify app.tsx

--- plantree-web/src/App.tsx
+++ plantree-web/src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Notification } from 'bloomer';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+export function App() {
+  return (
+    <Container isFluid style={{ marginTop: 10 }}>
+      <Notification isColor="warning">
+        This container is <strong>fluid</strong>: it will have a 20px gap on either side.
+      </Notification>
+    </Container>
+  );
+}
